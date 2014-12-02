17285e8733ac12340d7d57b347bd59ed5d02b108
mdorchuck <mdorchuck@ic.sc.gov>
2014-12-02 14:41:03 -0500

    Update server.R

--- server.R
+++ server.R
@@ -2,8 +2,8 @@ library(shiny)
 library(PerformanceAnalytics)
 setwd(dir = "C:/Users/bloomberg/Documents/rdata")
 plan.ret = read.csv("planhistoricalreturns.csv")
-plan.ret$X = as.Date(x = plan.ret$X,format = "%m/%d/%Y")
-plan.ret.xts = as.xts(x = plan.ret[,2:14],order.by = plan.ret$X)
+plan.ret$Date= as.Date(x = plan.ret$Date,format = "%m/%d/%Y")
+plan.ret.xts = as.xts(x = plan.ret[,2:19],order.by = plan.ret$Date)
 # Define server logic required to plot relative performance
 shinyServer(function(input, output) {
   
@@ -17,11 +17,7 @@ shinyServer(function(input, output) {
   
   # Use table.Stats from Performance Analytics package
     table = table.Stats(R = plan.ret.xts,ci = .95)
-    table = table[13,]*sqrt(252)
-    View(table)
-    table = table[13]*sqrt(252)
-    table = table.Stats(R = plan.ret.xts,ci = .95)
-    table[13,] = sqrt(table[13,])*sqrt(252)
+    table[13,] = sqrt(table[13,])*sqrt(12)
   # to generate a table of summary statistics
   output$summary <- renderTable(table)
 })
